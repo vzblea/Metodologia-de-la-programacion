0d91f941f43d0c2156916a1617987e530b9d2491
vzblea <elianavazquez50@gmail.com>
2025-05-16 16:59:46 -0300

    Eliminados los archivos sueltos del root

--- Interface1.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Practica_4{
-	
-	public interface Comparable
-{
-    bool sosIgual(Comparable c);
-    bool sosMenor(Comparable c);
-    bool sosMayor(Comparable c);
-}
-}
--- OrdenAula2.cs
+++
@@ -1,9 +0,0 @@
-﻿﻿using System;
-namespace Practica_4
-	
-{
-	public interface OrdenAula2
-	{
-		void ejecutar(Comparable comp);
-	}
-}
